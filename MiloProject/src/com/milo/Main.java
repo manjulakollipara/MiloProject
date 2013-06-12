6f930778e9bb71e1ffa2cdd5b189dc4b6884fab9
ManjulaKollipara <manjusivak@gmail.com>
2013-06-12 05:45:27 -0700

    Uncommented the code to accept filePath as args[0]

--- MiloProject/src/com/milo/Main.java
+++ MiloProject/src/com/milo/Main.java
@@ -8,13 +8,13 @@ public class Main {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 
-		/*if( args.length < 1){
+		if( args.length < 1){
 			System.out.println("Please provide the input file");
 			return;
-		}*/
+		}
 		
-		String filePath= "src\\com\\test\\inputFile.txt";
-		//String filePath = args[0];
+		//String filePath= "src\\com\\test\\inputFile.txt";
+		String filePath = args[0];
 		MiloProcessFile mfo = new MiloProcessFile();
 		mfo.processFile(filePath);
 		
